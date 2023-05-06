dd47795de5dd797173e7083c46d6d0cd0a74b2b1
Swissky <12152583+swisskyrepo@users.noreply.github.com>
2023-05-06 12:38:34 +0200

    Upgrading rules

--- rules/parse.py
+++ rules/parse.py
@@ -1,15 +1,18 @@
 import yaml
 import sys
 import re
+import glob
 
-if len(sys.argv) < 2:
-    print(f"\nUsage:\n\t{sys.argv[0]} [regex-db.yml]")
-    exit(1)
+output = {}
 
-with open(sys.argv[1], 'r') as stream:
-    y = yaml.safe_load(stream)
+for yml in glob.glob('*.yml'):
+    with open(yml, 'r') as stream:
+        y = yaml.safe_load(stream)
 
-for i in y["patterns"]:
-    r = i["pattern"]["regex"]
-    name = i["pattern"]["name"]
-    print(f'"{name}" : `{r}`,')
+    for i in y["patterns"]:
+        r = i["pattern"]["regex"]
+        name = i["pattern"]["name"].replace(' ', '_').lower()
+        output[name] = r
+
+for line in output:
+    print(f'"{line}" : `{output[line]}`,')
